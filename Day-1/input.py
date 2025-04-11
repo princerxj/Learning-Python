716912e4d2886a36388c29d76793517bb7f0b5e2
princerxj <pr273582@gmail.com>
2025-04-11 21:24:14 +0530

    Taking Inputs

---
+++ Day-1/input.py
@@ -0,0 +1,15 @@
+# name = input("Enter your name : ")
+# print("Welcome ", name)
+# age = input("Enter you age : ")
+# print("Your age is : ", age)
+
+# By default : inout will be of String datatype
+# if we want to take input in specific datatype : we need to do like this :: int(input(" "))
+
+name = input("Enter your name : ")
+age = int(input("Enter your age : "))
+marks = float(input("Enter your marks : "))
+
+print("Welcome ", name)
+print("Age = ", age)
+print("Marks obtained : ", marks)
