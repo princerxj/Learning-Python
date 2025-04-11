fd269b7cf6f582c709c10797df5cf83ed9bfede8
princerxj <pr273582@gmail.com>
2025-04-11 20:51:41 +0530

    Basic Variables and Datatypes

---
+++ Day-1/FirstProgram.py
@@ -0,0 +1,15 @@
+# print("Hello World") - Hello World
+
+name = "Prince"  # Strings should be in double quotes
+age = 23
+price = 25.99
+
+print(name)  # Prints the value of the name
+print("name")  # prints the string "name"
+
+print("My name is : ", name)
+print("my age is : ", age)
+
+print(type(name))
+print(type(age))
+print(type(price))
---
+++ Day-1/readme.md
@@ -0,0 +1,2 @@
+In this , i read about the variables and different types of datatypes present in Python , 
+- .py - Extension for the python files 
