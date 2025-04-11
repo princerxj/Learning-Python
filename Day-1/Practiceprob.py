4aace4fa5d2d16f8a805df444a91c33cfaff88e0
princerxj <pr273582@gmail.com>
2025-04-11 21:33:58 +0530

    Practice Problems

---
+++ Day-1/Practiceprob.py
@@ -0,0 +1,19 @@
+# Write a program to input two numbers and print their sum
+a = int(input("Enter first number : "))
+b = int(input("Enter Second number : "))
+print("The sum of the numbers is :", a+b)
+
+# Write a program to input the side of a square and print its area
+side = float(input("Enter the length of each side : "))
+print("The area of the square is :", side**2)
+
+# Write a program to input 2 floating point numbers and print their average
+firstNum = float(input("Enter first number : "))
+secondNum = float(input("Enter second number : "))
+average = (firstNum + secondNum) / 2
+print("The average of the given numbers is :", average)
+
+# Write a program to take two numbers a and b as input and print true if a >= b else false
+a = int(input("Enter first Number :"))
+b = int(input("Enter second number :"))
+print(a >= b)
