b9bb8b3d173d597c935a1831bdf5800c622c0f58
princerxj <pr273582@gmail.com>
2025-04-11 21:08:16 +0530

    Type of Operators

---
+++ Day-1/operators.py
@@ -0,0 +1,34 @@
+a = 2
+b = 5
+# print(a+b)
+
+# Arithmetic Operators
+a = 5
+b = 8
+print(a+b)  # sum
+print(a-b)  # subtraction
+print(a % b)  # modulo Operator
+print(a / b)  # division
+print(a ** b)  # exponential
+print(a*b)  # multiplication
+
+
+# Relational Operators : Gives Boolean output as true or false
+a = 50
+b = 20
+print(a == b)  # == checks for the equality
+print(a != b)  # != : Checkis for the inequality
+print(a > b)
+print(a >= b)
+print(a < b)
+print(a <= b)
+
+# Assignment Operators : =, +=, -=, *=, /=, %=, **=
+a = 12
+a += 10  # Same as a = a + 10 , Same with the other operators
+
+# Logical Operators (not, and, or) : Operates on boolean values
+print(not False)
+print(not True)
+print(True and False)
+print(False or True)
