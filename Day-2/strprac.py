82248f7544dca5bb434f85a293859be99ac96976
princerxj <pr273582@gmail.com>
2025-04-12 21:19:13 +0530

    String Practice Questions

---
+++ Day-2/strprac.py
@@ -0,0 +1,7 @@
+# Write a program to input user first name and print its length
+name = input("Enter your first name : ")
+print(len(name))
+
+# Write a program to find the first occurence of $ in a string
+str = "Hello $obama"
+print(str.index("$"))
