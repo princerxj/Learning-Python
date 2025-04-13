8ac27e102162c3a0ef7935ca4b2fd5761c94f961
princerxj <pr273582@gmail.com>
2025-04-13 09:00:41 +0530

    Tuples in Python

---
+++ Day-3/tuples.py
@@ -0,0 +1,8 @@
+# Tuples
+# A built in datatype that lets us create immutable sequence of values
+# Example : tup = (87, 64, 33, 95, 76)
+# tup[0] = 43 , Not allowed
+# Slicing in tuples works same as of strings and lists
+tup = (2, 1, 3, 1)
+print(tup.index(1))  # returns index of first occurence
+print(tup.count(1))  # count total occurences
