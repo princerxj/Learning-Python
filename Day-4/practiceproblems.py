a6c3a42467603d65e33afe82ac00ad1cacff766a
princerxj <pr273582@gmail.com>
2025-04-14 08:28:34 +0530

    Practice Problems

---
+++ Day-4/practiceproblems.py
@@ -0,0 +1,14 @@
+# Store the following word meanings in a python dictionary :
+# 1.) table : "a piece of furniture"
+# 2.) cat : "a small animal"
+# 3.) census : "List of facts and figures"
+
+meaning = {
+    "table": "a piece of furniture",
+    "cat": "a small animal",
+    "census": "list of fact and figures",
+}
+print(meaning)
+
+# You are given a list of subjects of students . Assume one classroom is alloted for one subject , how many classes will be needed ?
+# Ans - Make a set , store the values of the subjects , some subjects would be common , so they gonna get ignored adding only unique values, the len of the set will be the answer.
