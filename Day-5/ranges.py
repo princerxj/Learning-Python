8df591b9c8b7fb6626e9a3a74188b5588e128b0d
princerxj <pr273582@gmail.com>
2025-04-15 09:04:24 +0530

    Ranges in Python

---
+++ Day-5/ranges.py
@@ -0,0 +1,10 @@
+# Range functions returns a sequence of numbers, starting from 0 by default,
+# and increments by 1(by default), and stops before a specified number.
+# Syntax : range(start?, stop, step?)
+
+for el in range(5):
+    print(el)
+for el in range(1, 5):
+    print(el)
+for el in range(1, 5, 2):
+    print(el)
