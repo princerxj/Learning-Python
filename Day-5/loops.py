55b7bf01e036416d9bfaf3405944670b1f9a2ffb
princerxj <pr273582@gmail.com>
2025-04-15 08:30:40 +0530

    While loops

---
+++ Day-5/loops.py
@@ -0,0 +1,8 @@
+# Loops are used to repeat the instructions
+# While loops :
+# Syntax : -> while condition :
+#                #some work
+count = 1
+while count <= 5:
+    print("Hello")
+    count += 1
