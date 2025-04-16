911cca014db84d7c8ab2c3b38b6d4a681d022b66
princerxj <pr273582@gmail.com>
2025-04-16 16:07:00 +0530

    Attributes

--- Day-8/init.py
+++ Day-8/init.py
@@ -2,6 +2,8 @@
 
 # creating class
 class Student:
+    college = "ABC college"
+
     def __init__(self, name, marks):
         self.name = name
         self.marks = marks
@@ -14,4 +16,9 @@ print(s1.name, s1.marks)
 s2 = Student("Arjun", 92)
 print(s2.name, s2.marks)
 
+print(Student.college)
+print(s1.college)
+print(s2.college)
 # The self param is a refernce to the current instance of the class and is used to access variables that belongs to the class
+# Class and Instance Attribites
+# class.attr, obj.attr
