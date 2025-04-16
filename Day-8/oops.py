63a93dacd7cf1a77f215fb16526169480af00a08
princerxj <pr273582@gmail.com>
2025-04-16 15:36:12 +0530

    Basics

---
+++ Day-8/oops.py
@@ -0,0 +1,21 @@
+# OOPs - To map with real world scenarios, we started using objects in code . That is called object Oriented Programming .
+# Class - blueprint for creating objects
+
+# Creating class
+class Student:
+    name = "Prince Raj"
+
+
+# creating object(instance)
+s1 = Student()
+print(s1.name)
+
+
+class Car:
+    name = "Tata"
+    Brand = "Mercedes"
+
+
+car1 = Car()
+print(car1.name)
+print(car1.Brand)
